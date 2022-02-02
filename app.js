1fe89d539375d50c6cff9237ea266136a33fd7ff
kaixuanzheng <63136439+kaixuanzheng@users.noreply.github.com>
2022-02-02 13:46:30 -0600

    Preparing a Codebase for Heroku Deployment

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/BlogDB');
+
+const postSchema = {
+  title: String,
+  content: String
+};
+const Post = mongoose.model("Post", postSchema);
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -22,22 +31,30 @@ app.use(express.static("public"));
 
 app.get('/posts/:title', function(req, res) {
   const requestTitle = _.lowerCase(req.params.title);
-  posts.forEach(function(post) {
-    const postTitle = _.lowerCase(post.title);
-    if (postTitle === requestTitle) {
-      res.render('post', {
-        title: post.title,
-        content: post.content
-      });
+  Post.find({}, function(err, posts) {
+    if (!err) {
+      posts.forEach(function(post) {
+        const postTitle = _.lowerCase(post.title);
+        if (postTitle === requestTitle) {
+          res.render('post', {
+            title: post.title,
+            content: post.content
+          });
+        }
+      })
     }
-  })
+  });
 })
 
 app.get('/', function(req, res) {
-  res.render('home', {
-    startingContent: homeStartingContent,
-    posts: posts
-  });
+  Post.find({}, function(err, posts) {
+    if (!err) {
+      res.render('home', {
+        startingContent: homeStartingContent,
+        posts: posts
+      });
+    }
+  })
 })
 
 app.get("/about", function(req, res) {
@@ -57,19 +74,24 @@ app.get("/compose", function(req, res) {
 })
 
 app.post('/compose', function(req, res) {
-  const post = {
+
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
-  }
-  posts.push(post);
-  res.redirect("/");
+  });
+
+  post.save(function(err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
 })
 
 
-
-
-
-
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
